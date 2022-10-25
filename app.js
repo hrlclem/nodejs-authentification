41f3e680ff61c9ee3a881221937a7ab7abf48b44
Clement <perso@clementhurel.fr>
2022-10-25 19:43:14 +0900

    set up password security and hashing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const mongoDb = "mongodb+srv://inventoryUsername:inventoryPassword@inventory-app.tdcky6s.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -32,10 +33,15 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
     });
   })
 );
@@ -53,6 +59,10 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get("/", (req, res) => {res.render("index", { user: req.user });});
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
@@ -66,15 +76,22 @@ app.get("/log-out", (req, res, next) => {
 });
 
 app.post("/sign-up", (req, res, next) => {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
-    }).save(err => {
-      if (err) { 
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if(err){
         return next(err);
       }
-      res.redirect("/");
-    });
+
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword
+      }).save(err => {
+        if (err) { 
+          return next(err);
+        }
+        res.redirect("/");
+      });
+
+    })
   });
 
 app.post("/log-in",
